03fe176a7524102ef4ca026ae02758e67b6d4086
Ocean <sudazhangyu@qq.com>
2016-08-06 17:10:24 +0800

    api 模块 和 文档模块

---
+++ portal-api/src/main/java/ren/maichu/portal/api/App.java
@@ -0,0 +1,13 @@
+package ren.maichu.portal.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
