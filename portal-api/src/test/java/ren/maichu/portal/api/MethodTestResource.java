86b3712391cd24263cdd565191c062e19fcea11f
Ocean <sudazhangyu@qq.com>
2016-08-27 14:30:14 +0800

    fix code error

--- portal-api/src/test/java/ren/maichu/portal/api/MethodTestResource.java
+++ portal-api/src/test/java/ren/maichu/portal/api/MethodTestResource.java
@@ -13,7 +13,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.xml.registry.infomodel.User;
 
 import org.springframework.stereotype.Service;
 
